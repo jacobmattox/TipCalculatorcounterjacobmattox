a7944c08cfcc9eff153b5e5ca88b4a765d04d25a
jacobmattox <jacob.mattox1017@gmail.com>
2019-02-12 18:49:54 -0500

    Initial commit

---
+++ app/src/main/java/com/introtoandroid/tipcalculator_counter_jacobmattox/MainActivity.java
@@ -0,0 +1,13 @@
+package com.introtoandroid.tipcalculator_counter_jacobmattox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
